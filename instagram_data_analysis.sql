ad5c6e8e9346574a318ad804497748826c50fa5b
Nurudeen Adesina <access2abey@hotmail.com>
2023-01-22 23:50:40 +0000

    creating users table

--- instagram_data_analysis.sql
+++ instagram_data_analysis.sql
@@ -1,3 +1,9 @@
 DROP DATABASE IF EXISTS instagram_db;
 CREATE DATABASE instagram_db;
 USE instagram_db; 
+
+CREATE TABLE users (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+);
