932c0c3da70067a17a4a5ef850538e8f36f9bfe4
Nurudeen Adesina <access2abey@hotmail.com>
2023-01-23 22:34:50 +0000

    creating photo_tags table

--- instagram_data_analysis.sql
+++ instagram_data_analysis.sql
@@ -55,4 +55,11 @@ CREATE TABLE tags (
   created_at TIMESTAMP DEFAULT NOW()
 );
 
+CREATE TABLE photo_tags (
+    photo_id INTEGER NOT NULL,
+    tag_id INTEGER NOT NULL,
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    FOREIGN KEY(tag_id) REFERENCES tags(id),
+    PRIMARY KEY(photo_id, tag_id)
+);
 
